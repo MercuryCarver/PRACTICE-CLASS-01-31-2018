7793197a48635e1df8fa1edd599d3e7fdd31266c
Mercury Carver <mercurycarver@gmail.com>
2018-01-31 12:55:21 -0500

    first commit

---
+++ src/Constructor2.java
@@ -0,0 +1,20 @@
+
+public class Constructor2 {
+
+
+		// TODO Auto-generated method stub
+String LaptopBrand;
+double price;
+int year;
+
+public Constructor2 (String LaptopBrand2, double price2, int year2) {
+	LaptopBrand = LaptopBrand2;
+	price = price2;
+	year = year2;
+	
+	
+}
+
+	}
+
+
---
+++ src/Constructor3.java
@@ -0,0 +1,12 @@
+
+public class Constructor3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Constructor2 constructor = new Constructor2("HP", 1400, 2017);
+System.out.println(constructor.LaptopBrand);
+Constructor2 laptop = new Constructor2("Dell", 1200, 2017);
+System.out.print(laptop.LaptopBrand + " " + laptop.price  );
+	}
+
+}
